8ede4ff0c9318fd180d932f76832b5c9306d4403
Nick <nickdloucks@outlook.com>
2022-01-07 08:46:07 -0800

    initial commit

---
+++ cash-register.js
@@ -0,0 +1,58 @@
+function checkCashRegister(price, cash, cid) {
+    // ========= DATA STORE ====
+    const MONEY = {
+        "PENNY": .01,
+        "NICKEL": .05,
+        "DIME": .10,
+        "QUARTER": .25,
+        "ONE": 1.00,
+        "FIVE": 5.00,
+        "TEN": 10.00,
+        "TWENTY": 20.00,
+        "ONE HUNDRED": 100.00
+    }
+    let changeDue = cash - price;
+    let tenderList = [];
+
+    function tillCount(arr2D){
+        let counter = 0;
+        for (let i=0; i < arr2D.length; i++){
+            counter += arr2D[i][1];
+        };
+        counter = Math.round(100 * counter) / 100;
+        return counter;
+    }
+    let totalTill = tillCount(cid);
+
+    //====BODY OF ALGORITHM============
+    if (totalTill < changeDue){
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+    } else if (totalTill === changeDue){
+        return {status: "CLOSED", change: cid};
+    } else {
+        cid = cid.reverse();
+        for (let tender of cid){
+            let currentTender = [tender[0], 0];
+            while ((changeDue >= MONEY[tender[0]]) && (tender[1] > 0)){
+                currentTender[1] += MONEY[tender[0]];
+                tender[1] -= MONEY[tender[0]];
+                changeDue -= MONEY[tender[0]];
+                changeDue = Math.round(100 * changeDue) / 100;
+            }
+            if(currentTender[1] > 0){
+                tenderList.push(currentTender);
+            }
+        }
+        console.log(...tenderList)
+    }
+
+    if (changeDue > 0){
+        console.log({status: "INSUFFICIENT_FUNDS", change: []})
+        return {status: "INSUFFICIENT_FUNDS", change: []};
+    }
+    console.log({status: "OPEN", change: tenderList})
+    return {status: "OPEN", change: tenderList};
+}
+  
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], 
+["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
