afd6b3ef44b42e60b1d22ea5fc6a36fa669223fc
Nick <nickdloucks@outlook.com>
2022-01-23 19:26:40 -0800

    abstract functional button component for controls

---
+++ libs/components/buttons.mjs
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ControlButton = function(func, name) {
+    
+
+    
+        return(
+            <>
+                <button onClick ={}>{buttonName}</button>
+            </>
+        );
+    
+}
+
+ControlButton.propTypes = {
+    functions: PropTypes.func.isRequired
+};
+export default ControlButton;
+  
