23983ee87b1f22519c702582ae8a68a13a14789d
Nick <nickdloucks@outlook.com>
2022-01-28 11:48:28 -0800

    init slotVal: total $ in current slot

--- src/recurseCount.js
+++ src/recurseCount.js
@@ -17,17 +17,19 @@ function recurseCount($owed, $index){
     // or there are no more types of money that could be given out
     let $type = MONEY[$index][0]; // alias for name of current bill/coin
     let $val = MONEY[$index][1]; // alias for value of current bill/coin
+
+    let slotVal = cid[$index][1]; // alias for total value of the money in the current bill/coin slot
     
     if($owed == $val){ // EVEN DIV CHANGE POP SUBROUTINE
         // the ammount still owed is equal to the value of the current bill/coin
         changePile.unshift([$type, $owed]); // add the $ name and value to the change pile to be given to customer
-        // ******cid[$index][1] -= $owed; // remove from till
+        cid[$index][1] -= $owed; // remove from till
         return;
     } else if ($owed > $val){ // CHANGE BUCKET POP SUBROUTINE:
         let remainder = $owed % $val; // change still due after grabbing some of the current bill/coin from till
         let give = $owed - remainder; // value of $ to be given from this slot (example: how much change in $1 bills if 1.00 is the current $val)
         changePile.unshift([$type, give]); // add the change to the pile to be given to the customer
-        // ******cid[$index][1] -= give; // remove from till
+        cid[$index][1] -= give; // remove from till
         recurseCount(remainder, $index - 1);
         return;
     } else { // $owed < $val
@@ -46,7 +48,8 @@ changePile.forEach(val => console.log(val));
  * 
  * Still need to be sure that it is only taking from what money is actually in the till.
  * 
- * 
+ * <slotVal> should NEVER become negative; 
+ * it should only be >= 0 and evenly divisible by 0.01
  * 
  * 
  */
