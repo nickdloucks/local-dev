25e589fffa847472d999c4878016c17cdde3730c
Nick <nickdloucks@outlook.com>
2022-02-23 19:55:25 -0800

    add public access methods, props private

--- src/libs/data-structures/a-node.ts
+++ src/libs/data-structures/a-node.ts
@@ -1,9 +1,28 @@
 export default class AbstractNode {
-    _alias: string;
-    _value : any;
+    private alias: string; // name of the node
+    private value: unknown; // value of the node
+
+    // public access methods:
+    public readonly getValue: unknown;
+    public setValue;
+    public readonly getAlias: unknown;
+    public setAlias;
 
     constructor (alias: string, val : any ){
-        this._alias = alias;
-        this._value = val;
+        this.alias = alias;
+        this.value = val;
+
+        this.getValue = function(): unknown{
+            return this.value;
+        }
+        this.setValue  = function(newVal: unknown): void {
+            this.value = newVal;
+        }
+        this.getAlias = function(): unknown{
+            return this.alias;
+        }
+        this.setAlias = function(newAlias: unknown):void {
+            this.alias = newAlias as string;
+        }
     }
 }
