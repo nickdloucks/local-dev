07ad36deecf09bf419861032937f5fe8b7bbba84
Nick <nickdloucks@outlook.com>
2022-02-23 20:06:12 -0800

    setup head and tail of linked list

--- src/libs/data-structures/linked-list.ts
+++ src/libs/data-structures/linked-list.ts
@@ -1,5 +1,13 @@
 import AbstractNode from "./a-node";
 
 export default class LinkedList {
-    
+    private head: null | AbstractNode;
+    private tail: null;
+
+    constructor(data?: Array<AbstractNode>){
+        this.head = (data) ? data[0]: null;
+        this.tail = null;
+
+        
+    }
 }
