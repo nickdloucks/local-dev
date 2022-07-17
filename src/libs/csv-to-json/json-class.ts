bfbced9996665f675956651af541d083c3e17b12
Nick <nickdloucks@outlook.com>
2022-07-17 08:26:28 -0700

    split data, start map func

--- src/libs/csv-to-json/json-class.ts
+++ src/libs/csv-to-json/json-class.ts
@@ -1,14 +1,22 @@
 class CSVObject {
-    rows: string[]
+    public rows: string[]
     headers: string[]
 
     constructor(data: string){
         this.rows = data.split('/n');
+        this.rows.forEach((row: string) =>{
+            row.split(','); // now a 2-D array of data points ?
+        })
         this.headers = this.rows[0].split(',');
     }
 
-    public toJSON = function(){
+    public toJSON = function(data: string[] = this.rows){
         // loop thru headers and create the fields.
+        let jsonObj = {}; 
+        data.forEach(row => {
+            // jsonObj['key'] = value
+        });
+
         // then assign all values to the correct headers and populate a matrix of row objects
     }
 }
