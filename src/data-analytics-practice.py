b126dfedfc8dfc107763b9b27ee2352d07b855b3
Nick <80440799+nickdloucks@users.noreply.github.com>
2022-08-13 16:02:29 -0700

    sports data api

--- src/data-analytics-practice.py
+++ src/data-analytics-practice.py
@@ -7,3 +7,4 @@ api_response = requests.get('url')
 #analyze some sports stats
 # https://developer.nfl.com/get-started/overview
 # https://gist.github.com/nntrn/ee26cb2a0716de0947a0a4e9a157bc1c
+# https://sportsdata.io/nfl-api
