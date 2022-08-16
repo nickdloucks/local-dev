d4c9c5d922724662e366cadc1a60b2532a513d18
Nick <80440799+nickdloucks@users.noreply.github.com>
2022-08-15 19:23:02 -0700

    api list for practice

--- src/data-analytics-practice.py
+++ src/data-analytics-practice.py
@@ -8,3 +8,6 @@ api_response = requests.get('url')
 # https://developer.nfl.com/get-started/overview
 # https://gist.github.com/nntrn/ee26cb2a0716de0947a0a4e9a157bc1c
 # https://sportsdata.io/nfl-api
+
+
+# https://www.springboard.com/blog/data-science/top-apis-for-data-scientists/
